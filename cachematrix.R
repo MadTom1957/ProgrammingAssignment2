dbc28b6f31cb1b751c842d8404184ad73cb38f24
Tom Couture <thomas.couture@mantech.com>
2015-11-21 22:43:39 -0500

    Update cachematrix.R
    I have never committed code before....

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function produces a list that is used by cacheSolve() to eliminate 
+## the need for calculating an inverse to the same matrix more than once. 
+## This function calculates the inverse only once and stores the value to be
+## recalled by cacheSolve()
+  
+makeCacheMatrix <- function(A = matrix())
+{
+	## The final list will contain four items:
+	## 1)	set = stores the value of matrix and the flag "m" in the parent environment 
+	## 2)	get = is the original matrix A
+	## 3)	setinverse contains the solve function
+	## 4)	getinverse holds the calculated inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(B)
+	{
+		A <<- B
+		m <<- NULL
+	}
+	get <- function() A
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set , get = get , setinverse = setinverse , getinverse = getinverse )
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if an inverse has been calculated and cached by
+## a previous function, "makeCacheMatrix()".  If the flag "m" is null, it 
+## calculates the inverse. If the flag is not NULL, it uses the stored cached
+## inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(A, ...)
+{
+	m <- A$getinverse()
+	if(!is.null(m))
+	{
+		message("...using cached inverse...")
+		return(m)
+	}
+	my_matrix <- A$get()
+	m <- solve(my_matrix, ...)
+	A$setinverse(m)
+	m
 }
